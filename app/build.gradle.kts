339ef19cc5df6531e8b0631f3665f347f061b16b
onlymash <im@fiepi.me>
2021-07-19 20:06:52 +0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "onlymash.materixiv"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 6
-        versionName = "0.0.3_beta01"
+        versionCode = 7
+        versionName = "0.0.3_beta02"
         renderscriptTargetApi = 21
         renderscriptSupportModeEnabled = false
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
